600080ec6d5cbd78041acfaf23e523e3091b5f6c
ssukhraj12 <ssukhraj12@gmail.com>
2025-12-10 11:46:25 +0530

    users export

--- resources/js/admin/AdminUsers.vue
+++ resources/js/admin/AdminUsers.vue
@@ -1,29 +1,26 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col cols="12" md="3">
-                <v-card color="teal">
-                    <v-card-text></v-card-text>
-                </v-card>
+        <v-row dense>
+            <v-col cols="12" md="6">
+                <h4 class="text-h5">Users List</h4>
+            </v-col>
+            <v-col cols="12" md="6" class="text-end">
+                <v-btn variant="outlined" class="text-none" color="grey-darken-4" density="compact"
+                       @click="exportToCSV">
+                    Export Users
+                </v-btn>
+            </v-col>
+            <v-col cols="12" md="9">
+                <v-text-field v-model="usearch" prepend-inner-icon="mdi-magnify" density="compact" variant="outlined"
+                              hide-details placeholder="Search Users"></v-text-field>
             </v-col>
             <v-col cols="12" md="3">
-                <v-card color="navy">
-                    <v-card-text></v-card-text>
-                </v-card>
-            </v-col>
-            <v-col cols="12" md="3">
-                <v-card color="mint">
-                    <v-card-text></v-card-text>
-                </v-card>
-            </v-col>
-            <v-col cols="12" md="3">
-                <v-card color="charcoal">
-                    <v-card-text></v-card-text>
-                </v-card>
+                <v-btn color="navy" density="default" block append-icon="mdi-plus">Add User</v-btn>
             </v-col>
             <v-col cols="12" md="12">
                 <v-card>
-                    <v-data-table :items="users" :headers="usersHeaders" density="comfortable" :hide-default-footer="users.length < 20">
+                    <v-data-table :items="users" :headers="usersHeaders" density="comfortable" :search="usearch"
+                                  :hide-default-footer="users.length < 10">
                         <template v-slot:item.email_verified_at="{item}">
                             <v-icon v-if="item.email_verified_at" color="green" title="verified">mdi-account-check</v-icon>
                             <v-icon v-else color="red" title="Unverified">mdi-account-question</v-icon>
@@ -58,6 +55,7 @@ export default {
     name:'AdminUsers',
     data(){
         return{
+            usearch:'',
             users:[],
             usersHeaders:[
                 {title:"Name",key:'name'},
@@ -79,6 +77,25 @@ export default {
                 .then((resp)=>{
                     this.users = resp.data.users;
                 })
+        },
+        exportToCSV() {
+            const headers = ['Name', 'Email', 'Role','Phone'];
+            const rows = this.users.map((user) => {
+                return [
+                    `${user.name}`,
+                    user.email || '',
+                    `${user?.role || 'user'}`,
+                    `${user?.phone || ' '}`
+                ];
+            });
+            const csvContent = [headers, ...rows].map(e => e.join(",")).join("\n");
+            const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.setAttribute("download", "users.csv");
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         }
     }
 }
